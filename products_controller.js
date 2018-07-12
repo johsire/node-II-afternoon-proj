034bd51af526af8d4050c873dc3e29dc91bad336
Josiah Maina <josiah5115@gmail.com>
2018-07-12 16:50:23 -0600

    created a variable for the db instance off of req.app

--- products_controller.js
+++ products_controller.js
@@ -4,18 +4,22 @@ modules.exports = {
   },
 
   getOne: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
   },
 
   getAll: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
   },
 
   update: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
   },
 
   delete: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
   }
 };
