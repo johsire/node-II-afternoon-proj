1665e8dcfde75711257d021ef5c8f0e003260244
Josiah Maina <josiah5115@gmail.com>
2018-07-12 16:38:34 -0600

    created tabl in heroku & corresponding db files w/ appropriate methods

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`Its going down on port ${port}.`);});
-
 massive(peocess.env.CONNECTION_STRING).then(dbInstance => {
   app.set('db', dbInstance)
 }).catch(err => console.log(err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log(`Its going down on port ${port}.`);});
