1edb7f6d2e7f4f06dbbb74b8037dc8a125042359
Josiah Maina <josiah5115@gmail.com>
2018-07-12 16:46:47 -0600

    created prods controller & initialized oue CRUD methods in modules.xprt

---
+++ products_controller.js
@@ -0,0 +1,21 @@
+modules.exports = {
+  create: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+  },
+
+  getOne: (req, res, next) => {
+
+  },
+
+  getAll: (req, res, next) => {
+
+  },
+
+  update: (req, res, next) => {
+
+  },
+
+  delete: (req, res, next) => {
+
+  }
+};
